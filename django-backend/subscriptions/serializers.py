ee1ea6707d8dc6f8f54b5e3238b1a7aa5a5efc0c
sanjai kumar n <nsksanjai@gmail.com>
2022-02-19 11:10:15 +0530

    55

--- django-backend/subscriptions/serializers.py
+++
@@ -1,9 +0,0 @@
-from rest_framework import serializers
-from .models import Subscription
-
-
-class SubscriptionSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Subscription
-        fields = ('name', 'description', 'created_at', 'updated_at'
-        )
--- subscription-app/src/main.js
+++ subscription-app/src/main.js
@@ -1,4 +1,20 @@
-import { createApp } from 'vue'
-import App from './App.vue'
+import Vue from "vue";
+import App from "./App.vue";
+import router from "./router";
+import store from "./store";
+import BootstrapVue from "bootstrap-vue";
+import VeeValidate from 'vee-validate';
 
-createApp(App).mount('#app')
+import "bootstrap/dist/css/bootstrap.min.css"
+import "bootstrap-vue/dist/bootstrap-vue.css"
+
+Vue.use(BootstrapVue)
+Vue.use(VeeValidate);
+
+Vue.config.productionTip = false;
+
+new Vue({
+  router,
+  store,
+  render: h => h(App)
+}).$mount("#app");
