c79a6399c08d35f6befc9e98174c10a572cf1d69
sanjai kumar n <nsksanjai@gmail.com>
2022-02-19 11:50:30 +0530

    888

--- subscription-app/src/App.vue
+++ subscription-app/src/App.vue
@@ -4,7 +4,8 @@
       <div class="col-md-8 main-header">
         <h2><router-link to="/index">Todo List</router-link></h2>
         <router-link to="/create" class="btn btn-sm btn-primary">Add Task</router-link>
-
+        <router-link to="/index" class="btn btn-sm btn-secondary task-b">Back to list</router-link>
+      
         <router-view />
       </div>
     </div>
@@ -20,6 +21,7 @@
     font-size: 40px;
     font-weight: bolder;
   }
+  
 @media only screen and (min-width: 768px) {
   .main-header{
     padding-top: 20px;
